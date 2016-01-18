57cafe512fa8d26e7495f37160ed018835503004
Michael Goodwin <mgoodwin@financeit.io>
2016-01-17 21:25:52 -0500

    WIP Copy over Trailblazer sign up tests.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,42 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+require "minitest/autorun"
+require "trailblazer/rails/test/integration"
 
-  # Add more helper methods to be used by all tests here...
+Rails.backtrace_cleaner.remove_silencers!
+
+Minitest::Spec.class_eval do
+  after :each do
+    # DatabaseCleaner.clean
+    Team.delete_all
+    User.delete_all
+  end
+end
+
+Cell::TestCase.class_eval do
+  include Capybara::DSL
+  include Capybara::Assertions
+end
+
+Trailblazer::Test::Integration.class_eval do
+  def sign_in!(email="fred@trb.org", password="123456")
+    sign_up!(email, password) #=> Session::SignUp
+
+    visit "/sessions/sign_in_form"
+
+    submit!(email, password)
+  end
+
+  def sign_up!(email="fred@trb.org", password="123456")
+    Session::SignUp::Admin.(user: {email: email, password: password})
+  end
+
+  def submit!(email, password)
+    within("//form[@id='new_session']") do
+      fill_in 'Email',    with: email
+      fill_in 'Password', with: password
+    end
+    click_button "Sign in!"
+  end
 end
