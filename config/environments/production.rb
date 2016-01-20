9711395040fe3d72845f2317b6dd94a39970c2bc
Michael Goodwin <mgoodwin@financeit.io>
2016-01-19 21:56:06 -0500

    Fiddle with assets config 3.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,13 +19,12 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
-  config.assets.precompile += %w( foundation_and_overrides.css.scss )
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
