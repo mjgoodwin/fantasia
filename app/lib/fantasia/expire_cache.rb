cc11cfc394d85dd521f6b6700deb90952fc7ddf7
Michael Goodwin <mgoodwin@financeit.io>
2016-02-06 08:07:44 -0500

    Setup infrastructure for timeago in cells

--- app/lib/fantasia/expire_cache.rb
+++ app/lib/fantasia/expire_cache.rb
@@ -1,7 +1,7 @@
-module Gemgem
+module Fantasia
   module ExpireCache # DISCUSS: could also be class.
     def expire_cache!(*)
-      CacheVersion.for("thing/cell/grid").expire! # of course, this is only temporary as it
+      CacheVersion.for("league/cell/grid").expire! # of course, this is only temporary as it
       # 1. binds Op to view.
       # 2. expires cache even if thing is not part of that screen.
     end
